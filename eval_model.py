da74487810b049fa26990cb2d222aeb7b600a7d4
Han Qi <qihan.dev@gmail.com>
2017-04-30 16:26:43 -0700

    Add model eval

--- eval_model.py
+++ eval_model.py
@@ -1,12 +1,40 @@
+import torch
+import pickle
+
+model_path = '.'
+validation_file = '.'
+features_file = '.'
 
 
-encoder_path = './encoder.pkl'
-decoder_path = './decoder.pkl'
+def make_mini_batch(image_features, image_data, batch_size=32, use_caption=None):
+    image_source = image_data.items()
+    random.shuffle(image_source)
+    if use_caption is None:
+        use_caption = random.randint(0, 4)
 
-def get_model(encoder_path, decoder_path):
-    encoder = EncoderCNN(4096, embed_dim)
-    decoder = DecoderRNN(embed_dim, hidden_size,
-                         len(vocab), num_layers_rnn)
+    for i in range(0, len(image_source), batch_size):
+        start = i
+        end = i + batch_size
+        if end >= len(image_source):
+            end = len(image_source)
+        feature_idx =[[x[1]['fid'] for x in
+                     image_source[start:end]]]
+        features = image_features[feature_idx]
+        captions = []
+        for _, x in image_source[start:end]:
+            captions.extend(x['caption'])
+        yield features, captions
+
 def main():
+    model = torch.load(model_path)
+
+    with open(validation_file) as f:
+        validation = pickle.load(f)
+    features = scipy.io.loadmat(features_file)
+
+    for x, y in make_mini_batch(features, validation):
+        print x, y
 
 
+if __name__ == '__main__':
+    main()
